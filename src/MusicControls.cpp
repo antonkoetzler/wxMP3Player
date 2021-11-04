0f4c87901b5d5d19653d4a1c4080f9e9923d2001
Anton Koetzler-Faust <anton.koetzlerfaust@uleth.ca>
2021-11-04 04:05:48 -0600

    Found how to center the music controls

--- src/MusicControls.cpp
+++ src/MusicControls.cpp
@@ -13,8 +13,10 @@ void MusicControls::loadMusicControls()
 
 	imageBitmap.LoadFile("../img/playButton.jpeg", wxBITMAP_TYPE_JPEG);
 
+	controlsSizer->AddStretchSpacer();
 	if (imageBitmap.IsOk())
-		controlsSizer->Add(new wxBitmapButton(this, PLAYBUTTON, imageBitmap), 0, wxCENTER);
+		controlsSizer->Add(new wxBitmapButton(this, PLAYBUTTON, imageBitmap, wxDefaultPosition, wxSize(80, 80)), 0, wxALIGN_CENTRE);
+	controlsSizer->AddStretchSpacer();
 	
 	this->SetSizer(controlsSizer);
 }
